ae61eb1e91c8ef677a298a4faf1a6cb035e564d2
katanamajesty <sasha.panasenk@gmail.com>
2025-06-22 22:00:51 +0100

    Hide old source

--- src/World/Components.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Components/CameraComponent.h"
-#include "Components/LightComponent.h"
-#include "Components/MeshComponent.h"
-#include "Components/NametagComponent.h"
-#include "Components/ParentComponent.h"
-#include "Components/TransformComponent.h"
